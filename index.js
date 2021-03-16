1eb6858f7027248addd74500f6c972b64f38d351
tino <vovthuy@gmail.com>
2021-03-16 11:51:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+const config = require('./config.json');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+});
+
+client.on('message', message => {
+	console.log(message.content);
+	console.log(config.secret_passcodes.bank);
+	console.log(config.secret_passcodes.home);
+});
+
+// login to Discord with your app's token
+client.login(config.token);
